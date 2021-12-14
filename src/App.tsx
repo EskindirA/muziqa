8687de899efef8b67f454dc3035e6effbcca6417
= <=>
2021-12-15 02:36:15 +0300

    lib setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import { RoundedButton } from "./components/RoundedButton/RoundedButton";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>MuziQa</p>
+        <RoundedButton />
       </header>
     </div>
   );
---
+++ src/components/RoundedButton/RoundedButton.tsx
@@ -0,0 +1,8 @@
+import { FC } from "react";
+import { RedRounded } from "./RoundedButton.styled";
+
+const RoundedButton: FC = () => {
+  return <RedRounded variant="contained">Muziqa Contained</RedRounded>;
+};
+
+export { RoundedButton };
